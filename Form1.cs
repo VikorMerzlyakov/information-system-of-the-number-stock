0d9c706416b56b65c7c2d4ba5f10e4814f65fbe4
VikorMerzlyakov <111169794+VikorMerzlyakov@users.noreply.github.com>
2025-04-17 12:45:49 +0300

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -11,41 +11,56 @@ using System.Windows.Forms;
 
 namespace WindowsFormsApp12
 {
+    // Главная форма приложения для авторизации пользователей
     public partial class Form1 : Form
     {
+        // Статические поля для хранения активного логина и роли пользователя
         static public string loginActive;
         static public string whois;
+
+        // Конструктор формы
         public Form1()
         {
             InitializeComponent();
         }
 
+        // Обработчик события загрузки формы
         private void Form1_Load(object sender, EventArgs e)
         {
+            // Установление соединения с базой данных
             DBconection.ConnectionDB();
         }
 
+        // Обработчик события нажатия кнопки для авторизации
         private void button1_Click(object sender, EventArgs e)
         {
+            // Проверка заполненности обязательных полей
             if (textBox1.Text != "" && textBox2.Text != "")
             {
+                // Вызов метода авторизации из класса Autorization
                 Autorization.Autorization1(textBox1.Text, textBox2.Text);
+                // Проверка роли пользователя после авторизации
                 switch (Autorization.Role)
                 {
                     case null:
                         {
-                            MessageBox.Show("Такого аккаунта не существует!", "Проверте данные и попробуйте снова!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                            // Отображение сообщения об ошибке, если аккаунт не существует
+                            MessageBox.Show("Такого аккаунта не существует!", "Проверьте данные и попробуйте снова!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                             break;
                         }
                     case "Админ":
                         {
+                            // Установка значений для активного логина, роли и имени пользователя
                             loginActive = textBox1.Text;
                             whois = "Администратор";
                             Autorization.User = textBox1.Text;
 
+                            // Получение фамилии пользователя
                             string familia = Autorization.AutorizacionName(textBox1.Text);
                             Autorization.Familia = familia;
+                            // Отображение приветственного сообщения для администратора
                             MessageBox.Show(familia + ", добро пожаловать в меню администратора!", "Успешно!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            // Скрытие текущей формы и открытие формы администратора
                             this.Hide();
                             FormAdmin admin = new FormAdmin();
                             admin.Show();
@@ -53,23 +68,27 @@ namespace WindowsFormsApp12
                         }
                     case "Менеджер":
                         {
+                            // Установка значений для активного логина, роли и имени пользователя
                             loginActive = textBox1.Text;
                             whois = "Менеджер";
                             Autorization.User = textBox1.Text;
 
+                            // Получение фамилии пользователя
                             string familia = Autorization.AutorizacionName(textBox1.Text);
                             Autorization.Familia = familia;
+                            // Отображение приветственного сообщения для менеджера
                             MessageBox.Show(familia + ", добро пожаловать в меню менеджера!", "Успешно!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            // Скрытие текущей формы и открытие формы менеджера
                             this.Hide();
                             FormManager manager = new FormManager();
                             manager.Show();
                             break;
                         }
                 }
-
             }
             else 
             {
+                // Отображение сообщения о необходимости заполнения всех обязательных полей
                 MessageBox.Show("Заполните все обязательные поля!", "Заполнение полей", MessageBoxButtons.OK, MessageBoxIcon.Warning);
             }
         }
