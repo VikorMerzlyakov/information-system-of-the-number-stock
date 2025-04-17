12824403a1d1c67e807f47200493b5bbfb878640
VikorMerzlyakov <111169794+VikorMerzlyakov@users.noreply.github.com>
2025-04-17 12:42:09 +0300

    Update Cliner.cs

--- Cliner.cs
+++ Cliner.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,18 +10,23 @@ using System.Windows.Forms;
 
 namespace WindowsFormsApp12
 {
+    // Класс формы для назначения уборки номеров
     public partial class Cliner : Form
     {
+        // Конструктор формы
         public Cliner()
         {
             InitializeComponent();
         }
 
+        // Обработчик события нажатия кнопки для назначения уборки
         private void button1_Click(object sender, EventArgs e)
         {
+            // Проверка заполненности обязательных полей
             if (dayBox.Text != "" && monthBox.Text != "" && yearBox.Text != "" && numBox.Text != "" && comboBox1.Text != "")
             {
                 string cline;
+                // Определение типа уборки на основе выбранного значения в comboBox1
                 if (comboBox1.Text == "Генеральная уборка")
                 {
                     cline = "Генеральная_уборка";
@@ -30,22 +35,24 @@ namespace WindowsFormsApp12
                 {
                     cline = "Стандартная_уборка";
                 }
+                // Попытка добавить запись о уборке в базу данных
                 if (Admin.AddCline(Convert.ToInt32(dayBox.Text), Convert.ToInt32(monthBox.Text), Convert.ToInt32(yearBox.Text), Convert.ToInt32(numBox.Text), cline))
                 {
+                    // Отображение сообщения о успешном назначении уборки
                     MessageBox.Show("Уборка номера успешно назначена!", "Поздравляю!", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                    //ManagerClass.GetClient();
+                    // ManagerClass.GetClient(); // Возможный вызов метода для получения клиента, закомментирован
                 }
                 else
                 {
+                    // Отображение сообщения о неудачном назначении уборки
                     MessageBox.Show("Уборка не была назначена!");
                 }
-                
             }
             else
             {
+                // Отображение сообщения о необходимости заполнения всех обязательных полей
                 MessageBox.Show("Заполните все обязательные поля!");
             }
-
         }
     }
 }
